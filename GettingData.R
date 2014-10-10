6e29774fb9c48fb0a39132a5355dfe021ac02376
marciorbe <marciorbe@gmail.com>
2014-10-10 13:49:58 -0300

    Quiz 1

--- GettingData.R
+++ GettingData.R
@@ -87,6 +87,57 @@ tables()
 ### Subsetting rows
 DT[2,]
 DT[DT$y=="a",]
+### Subsetting rows
+DT[c(2,3)]
+### Subsetting columns!?
+DT[,c(2,3)] ## will not work!
+### Column subsetting in data.table
+#### The subsetting function is modified for data.table
+#### The argument you pass after the comma is called an "expression"
+#### In R an expression is a collection of statements enclosed in curley brackets
+{
+  x = 1
+  y = 2
+}
+k = {print(10); 5}
+## [1] 10
+print(k)
+## [1] 5
+## Calculating values for variables with expressions
+DT[,list(mean(x),sum(z))]
+DT[,table(y)]
+### Adding new columns
+DT[,w:=z^2]
+DT2 <- DT
+DT[, y:= 2]
+### Careful
+head(DT,n=3)
+head(DT2,n=3)
+### Multiple operations
+DT[,m:= {tmp <- (x+z); log2(tmp+5)}]
+### plyr like operations
+DT[,a:=x>0]
+DT[,b:= mean(x+w),by=a]
+### Special variables
+#### .N An integer, length 1, containing the numbe r
+set.seed(123);
+DT <- data.table(x=sample(letters[1:3], 1E5, TRUE))
+DT[, .N, by=x]
+### Keys
+DT <- data.table(x=rep(c("a","b","c"),each=100), y=rnorm(300))
+setkey(DT, x)
+DT['a']
+### Joins
+DT1 <- data.table(x=c('a', 'a', 'b', 'dt1'), y=1:4)
+DT2 <- data.table(x=c('a', 'b', 'dt2'), z=5:7)
+setkey(DT1, x); setkey(DT2, x)
+merge(DT1, DT2)
+### Fast reading
+big_df <- data.frame(x=rnorm(1E6), y=rnorm(1E6))
+file <- tempfile()
+write.table(big_df, file=file, row.names=FALSE, col.names=TRUE, sep="\t", quote=FALSE)
+system.time(fread(file))
+system.time(read.table(file, header=TRUE, sep="\t"))
 
 
 
