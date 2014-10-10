053b8bf51074950e952470132fc795597e824f76
marciorbe <marciorbe@gmail.com>
2014-10-10 12:53:16 -0300

    Quiz 1

--- GettingData.R
+++ GettingData.R
@@ -28,6 +28,67 @@ dateDownloaded <- date()
 library(xlsx)
 cameraData <- read.xlsx("./data/cameras.xlsx",sheetIndex=1,header=TRUE)
 head(cameraData)
+colIndex <- 2:3
+rowIndex <- 1:4
+cameraDataSubset <- read.xlsx("./data/cameras.xlsx",sheetIndex=1,
+                              colIndex=colIndex,rowIndex=rowIndex)
+cameraDataSubset
+
+## Reading XML files
+##Read the file into R
+library(XML)
+fileUrl <- "http://www.w3schools.com/xml/simple.xml"
+doc <- xmlTreeParse(fileUrl,useInternal=TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+names(rootNode)
+## Directly access parts of the XML document
+rootNode[[1]]
+rootNode[[1]][[1]]
+## Programatically extract parts of the file
+xmlSApply(rootNode,xmlValue)
+## Programatically extract parts of the file
+xmlSApply(rootNode,xmlValue)
+## Get the items on the menu and prices
+xpathSApply(rootNode,"//name",xmlValue)
+xpathSApply(rootNode,"//price",xmlValue)
+## Extract content by attributes
+fileUrl <- "http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+doc <- htmlTreeParse(fileUrl,useInternal=TRUE)
+scores <- xpathSApply(doc,"//li[@class='score']",xmlValue)
+teams <- xpathSApply(doc,"//li[@class='team-name']",xmlValue)
+scores
+teams
+
+## Reading JSON
+### Reading data from JSON {jsonlite package}
+library(jsonlite)
+jsonData <- fromJSON("https://api.github.com/users/jtleek/repos")
+names(jsonData)
+### Nested objects in JSON
+names(jsonData$owner)
+jsonData$owner$login
+### Writing data frames to JSON
+myjson <- toJSON(iris, pretty=TRUE)
+cat(myjson)
+### Convert back to JSON
+iris2 <- fromJSON(myjson)
+head(iris2)
+
+## Using data.table
+### Create data tables just like data frames
+library(data.table)
+DF = data.frame(x=rnorm(9),y=rep(c("a","b","c"),each=3),z=rnorm(9))
+head(DF,3)
+DT = data.table(x=rnorm(9),y=rep(c("a","b","c"),each=3),z=rnorm(9))
+head(DT,3)
+### See all the data tables in memory
+tables()
+### Subsetting rows
+DT[2,]
+DT[DT$y=="a",]
+
+
 
 ## Quiz 1
 if (!file.exists("data")){
